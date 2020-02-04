dad662e78bacf65be5207ea213cb9fe9750db2ab
leonardo Kaplan <kaplanleonardo@gmail.com>
2020-02-04 17:12:01 -0600

    completed up to step 70, beginning to hook up spotify API functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,66 @@
 import React from 'react';
 import './App.css';
 
+import SearchBar     from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist      from '../Playlist/Playlist';
+
 class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      searchResults: [{name:'name1', artist:'artist1', album: 'album1', id:1},
+                      {name:'name2', artist:'artist2', album: 'album2', id:2},
+                      {name:'name3', artist:'artist3', album: 'album3', id:3}],
+      playlistName:  "Leo's Playlist",
+      playlistTracks: [{name:'PLaylistname1', artist:'PLaylistartist1', album: 'PLaylistalbum1', id:4},
+                       {name:'PLaylistname2', artist:'PLaylistartist2', album: 'PLaylistalbum2', id:5},
+                       {name:'PLaylistname3', artist:'PLaylistartist3', album: 'PLaylistalbum3', id:6}]
+    }
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist;
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track){
+    let tracks = this.state.playlistTracks
+    if (tracks.find(savedTrack => savedTrack.id === track.id)){
+      return;
+    }
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track){
+    let tracks = this.state.playlistTracks
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id)
+    this.setState({playlistTracks: tracks});
+  }
+  updatePlaylistName(name){
+    this.setState({playlistName: name});
+  }
+  savePlaylist(){
+    const trackURIs = this.state.playlistTracks.map(track => track.uri)
+  }
+  search(term){
+    console.log(term);
+  }
+
   render(){
     return (
       <div>
-        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}
+                      onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist}/>
           </div>
         </div>
      </div>
